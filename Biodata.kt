72078e728a22d1baddc4d95a85334714c4a8151c
J4rrr <145539362+J4rrajah@users.noreply.github.com>
2023-09-20 15:06:57 +0700

    Create Biodata.kt

---
+++ Biodata.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+  var nama = "Fajar Kurniawan"
+  var kelas = "XI RPL 2"
+  var hobi = "Write code"
+
+  println("Nama: $nama")
+  println("kelas: $kelas")
+  println("Hobi: $hobi")
+}
